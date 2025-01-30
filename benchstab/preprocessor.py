0776f1bf895d2904f326f46ab5abee88e6d739b9
Jan Velecký <xvelec07@vutbr.cz>
2025-01-30 15:14:13 +0100

    Fixed typo in an error message & comments

--- benchstab/preprocessor.py
+++ benchstab/preprocessor.py
@@ -222,7 +222,7 @@ class Preprocessor:
         
         If the fasta identifier is valid, return:
             * Fasta object.
-            * mutation in the format WT_RESIUDE + POSITION + MUT_RESIDUE.
+            * mutation in the format WT_RESIDUE + POSITION + MUT_RESIDUE.
             * pH (default: 7.0 if not supplied).
             * temperature (default: 25.0 if not supplied).
 
@@ -311,7 +311,7 @@ class Preprocessor:
         :type mutation: str
         :param fasta: Fasta record
         :type fasta: Fasta
-        :return: Mutation string in the format WT_RESIUDE + POSITION + MUT_RESIDUE
+        :return: Mutation string in the format WT_RESIDUE + POSITION + MUT_RESIDUE
         :rtype: str
         """
         return self.__exception_wrapper(
@@ -324,7 +324,7 @@ class Preprocessor:
         
         If the protein identifier is valid, return:
             * PDB object
-            * mutation in the format WT_RESIUDE + POSITION + MUT_RESIDUE
+            * mutation in the format WT_RESIDUE + POSITION + MUT_RESIDUE
             * chain
             * fasta object
             * pH (default: 7.0 if not supplied)
@@ -390,7 +390,7 @@ class Preprocessor:
 
         :param mutation: Mutation string to be parsed
         :type mutation: str
-        :return: Mutation string in the format WT_RESIUDE + POSITION + MUT_RESIDUE
+        :return: Mutation string in the format WT_RESIDUE + POSITION + MUT_RESIDUE
         :rtype: str
         """
         try:
@@ -401,7 +401,7 @@ class Preprocessor:
             raise PreprocessorError(
                 (
                     f'Mutation "{mutation}" has invalid format.'
-                    'Only accepted format is: WT_RESIUDE + POSITION + MUT_RESIDUE.'
+                    'Only accepted format is: WT_RESIDUE + POSITION + MUT_RESIDUE.'
                 )
             ) from exc
         # Invalid Wild Type residue
@@ -427,7 +427,7 @@ class Preprocessor:
         ) -> str:
         """
         Parse the mutation string and check if it is valid. If the mutation is valid,
-        return the mutation string in the format WT_RESIUDE + POSITION + MUT_RESIDUE.
+        return the mutation string in the format WT_RESIDUE + POSITION + MUT_RESIDUE.
 
         As this function also handles the parsing of the mutation string for the fasta
         record extracted from PDBs, it is possible that the mutation string is not valid.
@@ -441,7 +441,7 @@ class Preprocessor:
         :type fasta: Fasta
         :param permissive: If True, the function will raise a PreprocessorError with the permissive flag set to True
         :type permissive: bool
-        :return: Mutation string in the format WT_RESIUDE + POSITION + MUT_RESIDUE
+        :return: Mutation string in the format WT_RESIDUE + POSITION + MUT_RESIDUE
         :rtype: str
         """
         try:
@@ -456,7 +456,7 @@ class Preprocessor:
             raise PreprocessorError(
                 (
                     f'Mutation "{mutation}" has invalid format.'
-                    'Only accepted format is: WT_RESIUDE + POSITION + MUT_RESIDUE.'
+                    'Only accepted format is: WT_RESIDUE + POSITION + MUT_RESIDUE.'
                 )
             ) from exc
         except LookupError as exc:
@@ -504,7 +504,7 @@ class Preprocessor:
         
         If the protein identifier is valid, return:
             * PDB object
-            * mutation in the format WT_RESIUDE + POSITION + MUT_RESIDUE
+            * mutation in the format WT_RESIDUE + POSITION + MUT_RESIDUE
             * chain
             * fasta object
             * pH (default: 7.0 if not supplied)
